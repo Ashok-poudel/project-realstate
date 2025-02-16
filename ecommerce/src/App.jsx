51aac31baae67e6b1c7168d675dc2ab2ee4fe554
Ashok-poudel <172118673+Ashok-poudel@users.noreply.github.com>
2025-02-16 12:17:37 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ ecommerce/src/App.jsx
@@ -0,0 +1,31 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Compaines from "./components/Companies/Compaines";
+import Residences from "./components/Residences/Residences";
+import './App.css';
+import Value from "./components/Value/Value";
+import Contact from "./components/Contact/Contact";
+import GetStarted from "./components/GetStarted/GetStarted";
+import Footer from "./components/Footer/Footer";
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div className="white-gradient" /> 
+          <Header/>
+          <Hero/>
+        
+     
+          
+      </div>
+          <Compaines/>
+          <Residences/>
+          <Value/>
+          <Contact/>
+          <GetStarted/>
+          <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ecommerce/src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      " We offer competitive interest rates designed to maximize your returns and help you grow your savings effortlessly.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Our strategies focus on maintaining price stability, ensuring a secure and predictable financial environment for our customers.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "We guarantee the most competitive prices, giving you the best value without compromising on quality or service.",
+  },
+];
+export default data;
